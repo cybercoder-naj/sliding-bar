34a018e80ffbbea7ac23f5b109b470015648ebf1
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-05-29 16:00:06 +0530

    Starter code

---
+++ sliding-bar/src/main/java/me/nishant/sliding_bar/SlidingBar.kt
@@ -0,0 +1,9 @@
+package me.nishant.sliding_bar
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SlidingBar() {
+    Text(text = "Hello There!")
+}
